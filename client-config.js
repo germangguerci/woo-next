60b26d31d5588e26d73f1508214b2bbcc3ea006b
Germangguerci <germangguerci@gmail.com>
2020-11-07 20:58:50 -0300

    Removed, git ignore on config files

---
+++ client-config.js
@@ -0,0 +1,8 @@
+const clientConfig = {
+  siteUrl: 'http://localhost/devplugtest/', // e.g. 'http://xyz.com' - Your WordPress site URL.
+  graphqlUrl: 'http://localhost/devplugtest/graphql', // e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
+  productImagePlaceholder: 'https://via.placeholder.com/434', // e.g https://via.placeholder.com/434 - Placeholder image URL for index page
+  singleImagePlaceholder: 'https://via.placeholder.com/200', // e.g https://via.placeholder.com/200 - Placeholder image URL for individual product page
+};
+
+export default clientConfig;
---
+++ wooConfig.js
@@ -0,0 +1,6 @@
+const wooConfig = {
+	siteUrl: 'http://localhost/devplugtest/', // e.g. 'http://xyz.com' - Your WordPress site URL.
+	graphqlUrl: 'http://localhost/devplugtest/graphql' // e.g. 'http://wordpressSiteUrl.com/graphql' - Your WordPress site url followed by '/graphql'
+};
+
+module.exports = wooConfig;
